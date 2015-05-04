3dea382daad237ba37a1ed64af9b699c3e127828
Daisuke Fujita <dtanshi45@gmail.com>
2015-05-04 22:26:22 +0900

    Create OrgFile class as test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,16 @@ CodeClimate::TestReporter.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'organicat'
+
+require "tempfile"
+
+class OrgFile
+  class << self
+    def create
+      Tempfile.open("orgfile") do |f|
+        f.puts(yield)
+        f.path
+      end
+    end
+  end
+end
